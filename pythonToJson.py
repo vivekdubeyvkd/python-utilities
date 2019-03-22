a0058922ed24afe4540849a46744148c40dd6f82
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-03-22 17:27:00 +0530

    Create pythonToJson.py

---
+++ pythonToJson.py
@@ -0,0 +1,10 @@
+import json
+
+# a python dict
+x = {'arr': "3", 'gcr' : "1", 'cdr': "3", 'das' : "1", 'err': "2", 'atr' : "1"}
+
+# convert into json
+jsonData = json.dumps(x)
+
+#print json jsonData
+print(jsonData)
