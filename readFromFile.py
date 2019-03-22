556301c396e7d0aca4b83102799bae495592f7fa
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-03-22 17:40:08 +0530

    Create readFromFile.py

---
+++ readFromFile.py
@@ -0,0 +1,15 @@
+f = open('abc.txt', 'r')
+
+# print all content in one go
+print(f.read())
+
+# read first 10 characters
+#print(f.read(10))
+
+# loop through line by line
+#for line in f:
+#    print line
+
+# read one line at a time
+#print(f.readline())
+#print(f.readline())
