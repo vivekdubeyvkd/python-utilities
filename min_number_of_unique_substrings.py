8307f45fd0b5cd202bbb17e3d682b3dc346e0a26
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2022-06-12 11:58:40 +0530

    adding new script to split a string into min number of substrings with unique chars

---
+++ min_number_of_unique_substrings.py
@@ -0,0 +1,20 @@
+inputStr = "abacdec"
+
+def findMinSubstring(inpStr):
+	counter = 0
+	uniqueSubStringCount = 1
+	uniqueCountSet = set()
+	
+	while counter < len(inpStr):
+		if inpStr[counter] not in uniqueCountSet:
+			uniqueCountSet.add(inpStr[counter])
+		else:
+			uniqueCountSet = set()
+			uniqueCountSet.add(inpStr[counter])
+			uniqueSubStringCount += 1
+
+		counter += 1	
+	return 	uniqueSubStringCount
+	
+
+print(findMinSubstring(inputStr))			
